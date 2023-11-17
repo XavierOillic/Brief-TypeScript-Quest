016fa60faf835edc99d91390c6f95b7e4b5bab80
Xavier OILLIC <xavieroillic@gmail.com>
2023-11-17 22:53:38 +0100

    balin et legolas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 class Hero {
+    breed: string;
     name: string;
     power: number;
     life: number;
 
-    Constructor () {
-        this.name = name;
-        this.power = power;
-        this.life = life;
-    }
+
+  // Normal signature with defaults
+  constructor(breed : string, name : string, power : number, life : number ) {
+    this.breed = breed;
+    this.name = name;
+    this.power = power;
+    this.life = life;
+  } 
+
+    balin(opponent : Hero) {
+        if (this.power < this.life) {
+            return this.isAlive(`...still Alive, Dwarf !!`);
+        } else if (this.power > this.life){
+            return this.isAlive("eeeeeeuuuuuurgh......");
+        }
+  }
+
+  isAlive(result : string) {
+
+  };
 }
+
+const balin = new Hero("Dwarf","Bâlin", 750, 700);
+const legolas = new Hero("Elfe","Legolas", 1000, 600)
